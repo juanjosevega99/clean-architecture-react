5a0da280d702c831dbaa38da1a89846478e4ffad
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2022-12-12 22:44:20 -0500

    feat: main page

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,11 @@
+import { Layout } from '@/styled-components';
+
+import { HomeTitle } from './styled-components';
+
+export default function Home() {
+  return (
+    <Layout>
+      
+    </Layout>
+  )
+}
