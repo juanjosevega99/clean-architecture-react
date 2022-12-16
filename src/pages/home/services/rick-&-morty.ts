d1a94fa2326081beb6270c9b46c899ccd02be503
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2022-12-15 21:58:57 -0500

    feat: complete service rick and morty

--- src/pages/home/services/rick-&-morty.ts
+++ src/pages/home/services/rick-&-morty.ts
@@ -1,8 +1,9 @@
-// import {  } from "@/models";
-import {} from '../adapter';
+import { RickAndMortyAdapter } from '../adapter';
 
 export const rickAndMorty = 'https://rickandmortyapi.com/api/character/2';
 
 export const fetchMorty = async (url: string) => {
-  return fetch(url).then(res => res.json()).then(res => );
+  return fetch(url)
+    .then(res => res.json())
+    .then(res => RickAndMortyAdapter(res));
 };
